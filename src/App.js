3fc1674812b4c1940ab01b3a0e0407c8b3239136
Isra48 <israelalbarran48@gmail.com>
2021-11-19 09:11:31 -0600

    add sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import Marcian from "./marcian.svg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="Hero1">
+
+        <p className="pvalor">
+          La mejor forma de prepararte para la Universidad de tus sueños
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="title1">
+        Estudiar nunca fue <br/> tan divertido
+        </h1>
+
+      </div>
+
+
+      <div className="Hero2">
+        <div className="item1">1</div>
+        <div className="item2">2 aqui va mock up de app <br/> jojojoj
+        <h2>Hola</h2>
+        </div>
+        <div className="item3">6</div>
+        <div className="item4">3</div>
+        <div className="item5">5kjsdhfjkdshk</div>
+        <div className="item6">4</div>
+        <div className="item6">4</div>
+
+
+      </div>
+
+      <div className="section3">
+
+        <img src={Marcian} className="marcian" alt=""/>
+
+
+        <div className="contents3">
+          <h3>
+            Uniquiz
+          </h3>
+          <p>Una app creada por estudiantes para estudiantes que tambien realizaron el mismo examen de ingreso a la universidad.<br/> Estudia con nosotros de la forma más practica y accesible desde cualquier lugar, Ponte en forma con nuestro banco de preguntas estudiando cuando quieras... </p>
+
+          <button class="bubbly-button">Quizate!</button>
+          
+        </div>
+
+
+
+
+
+      </div>
+
+
+
+
+
+    
     </div>
   );
 }
